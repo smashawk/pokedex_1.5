01d9d4431740c826ef207562a9416f7bb2b6ffbb
daichi.tomonaga <jpx3033@linecorp.com>
2019-05-24 21:24:52 +0900

    ♻️ typeArray作成をfilter、mapメソッドで書き直す

--- src/js/common/createTypeArray.js
+++ src/js/common/createTypeArray.js
@@ -1,46 +1,44 @@
 import normalArray from '../common/createNormalArray'
 
+// タイプ検索で選ばれたタイプを持つポケモンデータが入った配列をつくる
 const createTypeArray = () => {
 
   const typeSelector1Value = document.getElementById('typeSelector1').value;
   const typeSelector2Value = document.getElementById('typeSelector2').value;
-  const typeArray = [];
-  const subTypeArray = [];
+  let typeArray = [];
+  let subTypeArray = [];
+
 
   // 単タイプもしくは指定なしの場合
   if( !(typeSelector1Value !== '-' && typeSelector2Value !== '-')) {
-    for(let i = 0; i<normalArray.length; i++){
-      for(let j = 0; j < 2; j++) {
-        if(normalArray[i].types[j] === typeSelector1Value || normalArray[i].types[j] === typeSelector2Value) {
-          typeArray.push({
-            key: i,
-            number: normalArray[i]
-          })
-        }
-      }
-    }
+
+
+    // 選ばれたタイプを持つポケモンの配列を作成
+    const originTypeArray = normalArray.filter(value => value.types[0] === typeSelector1Value || value.types[0] === typeSelector2Value || value.types[1] === typeSelector1Value || value.types[1] === typeSelector2Value);
+
+    // タイプ配列にkey値を追加
+    typeArray = originTypeArray.map((value, index) => ({
+      key: index,
+      number: value
+    }))
+
 
   } else {
+  // 複数タイプの場合
 
-    // 複数タイプの場合
-    for(let i = 0; i<normalArray.length; i++){
-      for(let j = 0; j < 2; j++) {
-        if(normalArray[i].types[j] === typeSelector1Value) {
-          subTypeArray.push({
-            key: i,
-            number: normalArray[i]
-          })
-        }
-      }
-    }
+    
+    // セレクタ1で選ばれたタイプを持つポケモンの配列を作成
+    subTypeArray = normalArray.filter(value => value.types[0] === typeSelector1Value || value.types[1] === typeSelector1Value);
 
-    for(let i = 0; i<subTypeArray.length; i++){
-      for(let j = 0; j < 2; j++) {
-        if(subTypeArray[i].number.types[j] === typeSelector2Value) {
-          typeArray.push(subTypeArray[i])
-        }
-      }
-    }
+    // セレクタ2で選ばれたタイプを持つポケモンの配列を作成
+    const originTypeArray = subTypeArray.filter(value => value.types[0] === typeSelector2Value || value.types[1] === typeSelector2Value);
+    
+    // タイプ配列にkey値を追加
+    typeArray = originTypeArray.map((value, index) => ({
+      key: index,
+      number: value
+    }))
+
 
   }
 
